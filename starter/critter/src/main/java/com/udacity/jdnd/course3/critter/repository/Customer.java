ba450ffdb851b9c7c823378aa3d9e59ab072ec9e
Juan Negrin <jcnegrin@icloud.com>
2021-04-20 13:05:29 +0200

    Added customer entity

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/Customer.java
@@ -0,0 +1,74 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Table
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized                   //support international language characters
+    private String name;
+    private String phoneNumber;
+    private String notes;
+
+    @OneToMany(targetEntity = Pet.class)
+    private List<Pet> pets;
+
+    public Customer(Long id, String name, String phoneNumber, String notes) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.notes = notes;
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+}
